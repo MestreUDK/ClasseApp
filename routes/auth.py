bbcbdd14b94b3ddab445131ae5e1e240dd8b2f9e
MestreUDK <117117483+MestreUDK@users.noreply.github.com>
2025-11-18 18:51:46 -0300

    Atualizar o auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -1,3 +1,5 @@
+# routes/auth.py
+
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required
 from utils import supabase
