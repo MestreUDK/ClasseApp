2ee6779b6c7d4300305bbd1db99a33becc0497c3
MestreUDK <117117483+MestreUDK@users.noreply.github.com>
2026-03-16 01:38:17 -0300

    Atualizar o utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,14 @@
 # utils.py
 import os
+from dotenv import load_dotenv
 from supabase import create_client, Client
 
-# Pega as chaves do Supabase das Variáveis de Ambiente
-SUPABASE_URL = os.environ.get("SUPABASE_URL")
-SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+# Carrega o .env
+load_dotenv()
+
+# Carrega as chaves do meu arquivo .env
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 # Cria o cliente Supabase e o exporta para outros arquivos usarem
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
